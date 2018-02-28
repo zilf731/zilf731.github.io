43a6aec91990058ad006190657bca505935cd3d6
zilf731 <36931575+zilf731@users.noreply.github.com>
2018-02-28 14:47:35 -0500

    test
    test

---
+++ src/gameobjects/rendertexture/RenderTexture.js
@@ -0,0 +1,78 @@
+var Class = require('../../utils/Class');
+var Components = require('../components');
+var GameObject = require('../GameObject');
+var RenderTextureWebGL = require('./RenderTextureWebGL');
+var RenderTextureCanvas = require('./RenderTextureCanvas');
+var Render = require('./RenderTextureRender');
+var CanvasPool = require('../../display/canvas/CanvasPool');
+
+var RenderTexture = new Class({
+
+    Extends: GameObject,
+
+    Mixins: [
+        Components.Alpha,
+        Components.BlendMode,
+        Components.Depth,
+        Components.Flip,
+        Components.GetBounds,
+        Components.MatrixStack,
+        Components.Origin,
+        Components.Pipeline,
+        Components.ScaleMode,
+        Components.ScrollFactor,
+        Components.Size,
+        Components.Tint,
+        Components.Transform,
+        Components.Visible,
+        Render
+    ],
+
+    initialize:
+
+    function RenderTexture(scene, x, y, width, height)
+    {
+        GameObject.call(this, scene, 'RenderTexture');
+        this.initMatrixStack();
+
+        this.renderer = scene.sys.game.renderer;
+        
+        if (this.renderer.type === Phaser.WEBGL)
+        {
+            var gl = this.renderer.gl;
+            this.gl = gl;
+            this.fill = RenderTextureWebGL.fill;
+            this.clear = RenderTextureWebGL.clear;
+            this.draw = RenderTextureWebGL.draw;
+            this.drawFrame = RenderTextureWebGL.drawFrame;
+            this.texture = this.renderer.createTexture2D(0, gl.NEAREST, gl.NEAREST, gl.CLAMP_TO_EDGE, gl.CLAMP_TO_EDGE, gl.RGBA, null, width, height, false);
+            this.framebuffer = this.renderer.createFramebuffer(width, height, this.texture, false);
+        }
+        else
+        {
+            this.fill = RenderTextureCanvas.fill;
+            this.clear = RenderTextureCanvas.clear;
+            this.draw = RenderTextureCanvas.draw;
+            this.drawFrame = RenderTextureCanvas.drawFrame;
+            this.canvas = CanvasPool.create2D(null, width, height);
+            this.context = this.canvas.getContext('2d'); 
+        }
+
+        this.setPosition(x, y);
+        this.setSize(width, height);
+        this.initPipeline('TextureTintPipeline');
+    },
+
+    destroy: function ()
+    {
+        GameObject.destroy.call(this);
+        if (this.renderer.type === Phaser.WEBGL)
+        {
+            this.renderer.deleteTexture(this.texture);
+            this.renderer.deleteFramebuffer(this.framebuffer);
+        }
+    }  
+
+});
+
+module.exports = RenderTexture;
---
+++ src/gameobjects/rendertexture/RenderTextureCanvasRenderer.js
@@ -0,0 +1,65 @@
+var GameObject = require('../GameObject');
+
+var RenderTextureCanvasRenderer = function (renderer, renderTexture, interpolationPercentage, camera)
+{
+    if (GameObject.RENDER_MASK !== renderTexture.renderFlags || (renderTexture.cameraFilter > 0 && (renderTexture.cameraFilter & camera._id)))
+    {
+        return;
+    }
+
+    var ctx = renderer.currentContext;
+
+    if (renderer.currentBlendMode !== renderTexture.blendMode)
+    {
+        renderer.currentBlendMode = renderTexture.blendMode;
+        ctx.globalCompositeOperation = renderer.blendModes[renderTexture.blendMode];
+    }
+
+    if (renderer.currentAlpha !== renderTexture.alpha)
+    {
+        renderer.currentAlpha = renderTexture.alpha;
+        ctx.globalAlpha = renderTexture.alpha;
+    }
+
+    if (renderer.currentScaleMode !== renderTexture.scaleMode)
+    {
+        renderer.currentScaleMode = renderTexture.scaleMode;
+    }
+
+    var dx = 0;
+    var dy = 0;
+
+    var fx = 1;
+    var fy = 1;
+
+    if (renderTexture.flipX)
+    {
+        fx = -1;
+        dx -= renderTexture.canvas.width - renderTexture.displayOriginX;
+    }
+    else
+    {
+        dx -= renderTexture.displayOriginX;
+    }
+
+    if (renderTexture.flipY)
+    {
+        fy = -1;
+        dy -= renderTexture.canvas.height - src.displayOriginY;
+    }
+    else
+    {
+        dy -= renderTexture.displayOriginY;
+    }
+
+    ctx.save();
+    ctx.translate(renderTexture.x - camera.scrollX * renderTexture.scrollFactorX, renderTexture.y - camera.scrollY * renderTexture.scrollFactorY);
+    ctx.rotate(renderTexture.rotation);
+    ctx.scale(renderTexture.scaleX, renderTexture.scaleY);
+    ctx.scale(fx, fy);
+    ctx.drawImage(renderTexture.canvas, dx, dy);
+    ctx.restore();
+
+};
+
+module.exports = RenderTextureCanvasRenderer;
